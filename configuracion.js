0841dbc39181bb3f83672567283296ce2092bde7
Isaac OBesso <isaacobesso2012@hotmail.com>
2022-03-10 17:40:48 -0500

    Arreglo en configuracion.js process.env.PORT

--- configuracion.js
+++ configuracion.js
@@ -3,7 +3,7 @@ const config = {
     DB_URL: 'mongodb+srv://ups:ups2020@cluster0.5vdk8.mongodb.net/ups_examen?retryWrites=true&w=majority',
     HOST: "pwp59-iobesso-mongodb-heroku.herokuapp.com",
     // HOST: "https://pwp59-iobesso-mongodb-heroku.herokuapp.com",
-    PUERTO: 3000
+    PUERTO: process.env.PORT || 3000
 }
 
 module.exports = config
